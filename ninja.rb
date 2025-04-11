719214262af38d98f8d98ada9e8aa9e86b039b81
realonecan <151053518+realonecan@users.noreply.github.com>
2025-04-11 11:59:30 +0500

    Update ninja.rb

--- ninja.rb
+++ ninja.rb
@@ -1,8 +1,8 @@
 class Ninja < Formula
   desc "Anonymous chat client"
   homepage "https://github.com/yourusername/ninjachat"
-  url "https://github.com/realonecan/ninjachat/releases/download/v1.0.0/ninja-darwin-amd64.tar.gz"
-  sha256 "your-sha256-checksum-here"
+  url "https://github.com/realonecan/ninja/releases/download/v1.0.0/ninja-darwin-amd64.tar.gz"
+  sha256 "3d6a4ae7f9c973c26ef3f706c22c04e9875aff3a93d5ec6dd9ebde3aed95dbc3" # Run `shasum -a 256 ninja-darwin-amd64.tar.gz`
   version "1.0.0"
 
   def install
